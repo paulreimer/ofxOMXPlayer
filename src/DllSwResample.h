d41d244fd840f63652a48c0446e9d5f8be38e10b
Paul Reimer <paul@p-rimes.net>
2014-02-10 10:35:20 -0800

    Removed const modifier for swr_convert wrapped call

--- src/DllSwResample.h
+++ src/DllSwResample.h
@@ -59,7 +59,7 @@ public:
   virtual struct SwrContext *swr_alloc_set_opts(struct SwrContext *s, int64_t out_ch_layout, enum AVSampleFormat out_sample_fmt, int out_sample_rate, int64_t in_ch_layout, enum AVSampleFormat in_sample_fmt, int in_sample_rate, int log_offset, void *log_ctx)=0;
   virtual int swr_init(struct SwrContext *s)=0;
   virtual void swr_free(struct SwrContext **s)=0;
-  virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, const uint8_t **in , int in_count)=0;
+  virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, uint8_t **in , int in_count)=0;
 };
 
 #if (defined USE_EXTERNAL_FFMPEG) || (defined TARGET_DARWIN) 
@@ -81,7 +81,7 @@ public:
   virtual struct SwrContext *swr_alloc_set_opts(struct SwrContext *s, int64_t out_ch_layout, enum AVSampleFormat out_sample_fmt, int out_sample_rate, int64_t in_ch_layout, enum AVSampleFormat in_sample_fmt, int in_sample_rate, int log_offset, void *log_ctx) { return ::swr_alloc_set_opts(s, out_ch_layout, out_sample_fmt, out_sample_rate, in_ch_layout, in_sample_fmt, in_sample_rate, log_offset, log_ctx); }
   virtual int swr_init(struct SwrContext *s) { return ::swr_init(s); }
   virtual void swr_free(struct SwrContext **s){ return ::swr_free(s); }
-  virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, const uint8_t **in , int in_count){ return ::swr_convert(s, out, out_count, in, in_count); }
+  virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, uint8_t **in , int in_count){ return ::swr_convert(s, out, out_count, in, in_count); }
 };
 #else
 // Wrap the same API through libavresample.
@@ -109,7 +109,7 @@ public:
   }
   virtual int swr_init(struct SwrContext *s) { return ::avresample_open(s); }
   virtual void swr_free(struct SwrContext **s){ ::avresample_close(*s); *s = NULL; }
-  virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, const uint8_t **in , int in_count){ return ::avresample_convert(s, (void**)out, 0, out_count, (void**)in, 0,in_count); }
+  virtual int swr_convert(struct SwrContext *s, uint8_t **out, int out_count, uint8_t **in , int in_count){ return ::avresample_convert(s, out, 0, out_count, in, 0,in_count); }
 };
 #endif
 
@@ -124,7 +124,7 @@ class DllSwResample : public DllDynamic, DllSwResampleInterface
   DEFINE_METHOD9(SwrContext*, swr_alloc_set_opts, (struct SwrContext *p1, int64_t p2, enum AVSampleFormat p3, int p4, int64_t p5, enum AVSampleFormat p6, int p7, int p8, void * p9));
   DEFINE_METHOD1(int, swr_init, (struct SwrContext *p1))
   DEFINE_METHOD1(void, swr_free, (struct SwrContext **p1))
-  DEFINE_METHOD5(int, swr_convert, (struct SwrContext *p1, uint8_t **p2, int p3, const uint8_t **p4, int p5))
+  DEFINE_METHOD5(int, swr_convert, (struct SwrContext *p1, uint8_t **p2, int p3, uint8_t **p4, int p5))
 
   BEGIN_METHOD_RESOLVE()
     RESOLVE_METHOD(swr_alloc_set_opts)
