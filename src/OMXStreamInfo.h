6450e2330b2e55124c48b0916e895fb01efbdc0f
Paul Reimer <paul@p-rimes.net>
2014-02-11 18:48:06 -0800

    Updating to some changes from latest omxplayer sources

--- src/OMXStreamInfo.h
+++ src/OMXStreamInfo.h
@@ -24,9 +24,13 @@
 #if (defined HAVE_CONFIG_H) && (!defined WIN32)
   #include "config.h"
 #endif
+#ifndef TARGET_LINUX
+enum AVCodecID;
+#else
 extern "C" {
   #include "libavcodec/avcodec.h"
 }
+#endif
 
 class CDemuxStream;
 
